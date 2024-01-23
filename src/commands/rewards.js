b0bcdbabadcfeab1baedf1e301cce697783b9d27
Min <kendamamin1480@gmail.com>
2024-01-23 08:08:24 -1000

    Making new rewards command for TL

---
+++ src/commands/rewards.js
@@ -0,0 +1,70 @@
+var { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+var { MongoClient } = require("mongodb");
+
+const setup = require('../../firstinit');
+
+var uri = "mongodb+srv://min:" + process.env.MONGODB_PASS + "@discord-seele.u4g75ks.mongodb.net/"
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('rewards')
+    .setDescription('COMMAND_DESCRIPTION'),
+
+    run: ({ interaction }) => {
+             
+        (async () => { // run, and if an error occurs, you can catch it
+
+            await interaction.deferReply();
+
+            // Placeholder embed for now
+            var testEmbed = new EmbedBuilder()
+            .setColor(0x9a7ee7)
+            .addFields(
+                {
+                    name: "\n",
+                    value: "\n"
+                },
+            )
+
+            try {
+
+                var client = new MongoClient(uri)
+
+                var database = client.db("economy");
+                var ids = database.collection("inventories")
+                var discordID = BigInt(interaction.user.id)
+
+                // Check how many documents are in the query (discord_id)
+                var counter = await ids.countDocuments({discord_id: discordID})
+
+                if (counter < 1) {
+                    // If document not found, make a new database entry, do this for all economy commands
+                    await setup.init(discordID, "economy", "inventories")
+                }
+                var options = {
+                    projection: {
+                        jade_count: 1
+                    }
+                }
+
+                // Then get the first thing that matches the discord id, and options is the query from before
+                var toParseUserUID = await ids.findOne({discord_id: discordID}, options);
+                var jade_count = toParseUserUID['jade_count']
+                
+                testEmbed.spliceFields(0, 1,
+                    {
+                        name: "\n",
+                        value: `to be implemented`
+                    })
+
+                interaction.editReply({ embeds: [testEmbed] });
+                await client.close()
+
+                } catch (error) {
+                    console.log(`There was an error: ${error.stack}`)
+                    interaction.editReply({ content: "Something broke!"})
+                    await client.close()
+                }
+        })();
+    }
+}
