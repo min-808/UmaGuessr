20c54a23d94f61995d818f5aeee81dbafd35e6f3
Min <kendamamin1480@gmail.com>
2025-08-07 00:38:14 -1000

    fixed negative daily

--- src/commands/daily.js
+++ src/commands/daily.js
@@ -44,23 +44,23 @@ module.exports = {
             var toParseUserUID = await ids.findOne({discord_id: discordID}, options);
             var pastTime = toParseUserUID['daily_timer']
 
-            let sec = Math.floor((pastTime - currentTime) / (1000))
-            let mins = Math.floor((pastTime - currentTime) / (1000 * 60))
-            let hours = Math.floor((pastTime - currentTime) / (1000 * 60 * 60))
-            let days = Math.floor((pastTime - currentTime) / (1000 * 60 * 60 * 24))
+            let sec = Math.floor((currentTime - pastTime) / (1000))
+            let mins = Math.floor((currentTime - pastTime) / (1000 * 60))
+            let hours = Math.floor((currentTime - pastTime) / (1000 * 60 * 60))
+            let days = Math.floor((currentTime - pastTime) / (1000 * 60 * 60 * 24))
 
-            if ((pastTime - currentTime) < 60_000) { // Seconds -> Minutes -> Hours -> Days
+            if ((currentTime - pastTime) < 60_000) { // Seconds -> Minutes -> Hours -> Days
                 writeTime = `${sec.toFixed(0)} seconds`
-            } else if ((pastTime - currentTime) < 3_600_000) {
+            } else if ((currentTime - pastTime) < 3_600_000) {
                 writeTime = `${mins.toFixed(0)} minutes and ${(sec - (mins * 60)).toFixed(0)} seconds`
-            } else if ((pastTime - currentTime) < 86_400_000) {
+            } else if ((currentTime - pastTime) < 86_400_000) {
                 writeTime = `${hours.toFixed(0)} hours, ${(mins - (hours * 60)).toFixed(0)} minutes, and ${(sec - (mins * 60)).toFixed(0)} seconds`
             } else {
                 writeTime = `${days.toFixed(0)} days`
             }
             
             // If you can't claim daily yet
-            if ((pastTime += 86_400_000) >= currentTime) {
+            if ((pastTime + 86_400_000) >= currentTime) {
                 embed.spliceFields(0, 1,
                     {
                         name: "\n",
