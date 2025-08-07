295b3be08a10b608f86e0adf3badae4199b558f1
Min <kendamamin1480@gmail.com>
2025-08-06 22:45:56 -1000

    added return message

--- src/commands/uma.js
+++ src/commands/uma.js
@@ -22,7 +22,7 @@ module.exports = {
         const channelID = message.channel.id;
 
         if (activeChannels.has(channelID)) {
-            return
+            return message.channelID.send("a game is running brochado");
         }
 
         activeChannels.add(channelID);
