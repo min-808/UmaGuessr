e6e6da0a03a4059605e867ba5d2283e5bcd6dc0f
Min <kendamamin1480@gmail.com>
2025-08-27 16:50:59 -1000

    test, added discord command

---
+++ src/commands/discord.js
@@ -0,0 +1,46 @@
+const { AttachmentBuilder, EmbedBuilder } = require('discord.js')
+var { MongoClient } = require("mongodb");
+
+const img = "discord"
+
+module.exports = {
+    name: 'discord',
+    aliases: ['dc'],
+    description: 'Get the link for the bot\'s discord server',
+    
+    run: async ({ message }) => {
+
+        try {
+            const file = new AttachmentBuilder(`src/assets/command_images/${img}.png`);
+
+            let embed;
+
+            embed = new EmbedBuilder()
+                .setColor('LightGrey')
+                .setTitle("Discord")
+                .setThumbnail(`attachment://${img}.png`)
+                .addFields(
+                    {
+                        name: "\n",
+                        value: `**Join the Discord Server!**\nhttps://discord.gg/d4rH6ycdbc`
+                    },
+                )
+
+            await message.channel.send({ embeds: [embed], files: [file] });
+        } catch (error) {
+            console.log(error.rawError.message) // log error
+
+            try {
+                await message.channel.send(`Unable to send embed: **${error.rawError.message}**\n\nPlease check the bot's permissions and try again`)
+            } catch (error) {
+                console.log(`Unable to send message: ${error.rawError.message}`)
+            }
+        } finally {
+            try {
+                await client_db.close()
+            } catch {
+                console.log("Couldn't close the connection")
+            }
+        }
+    }
+}
