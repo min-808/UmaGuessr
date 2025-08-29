be365ac7d5ff50fc7184f13473e2b538a7065cc7
Min <kendamamin1480@gmail.com>
2025-08-28 18:19:23 -1000

    Updated missing list

--- src/commands/missing.js
+++ src/commands/missing.js
@@ -17,33 +17,19 @@ module.exports = {
                     {
                         name: "\n",
                         value: 
-                        "Yamanin Zephyr\n" +
-                        "Wonder Acute\n" +
-                        "Hokko Tarumae\n" +
                         "Shinko Windy\n" +
-                        "Daiichi Ruby\n" +
-                        "Hishi Miracle\n" +
                         "Katsuragi Ace\n" +
-                        "K.S. Miracle\n" +
-                        "Mejiro Ramonu\n" +
                         "Tap Dance City\n" +
                         "Transcend\n" +
                         "Rhein Kraft\n" +
-                        "Sounds of Earth\n" +
                         "North Flight\n" +
                         "Calstone Light O\n" +
-                        "Cesario\n" +
-                        "Durandal\n" +
                         "Bubble Gum Fellow\n" +
                         "Air Messiah\n" +
-                        "Win Variation\n" +
                         "Furioso\n" +
                         "Gran Algeria\n" +
-                        "Fenomeno\n" +
                         "Loves Only You\n" +
-                        "Chrono Genesis\n" +
-                        "Fusaichi Pandora\n" +
-                        "Still in Love\n"
+                        "Fusaichi Pandora\n"
                     }
                 )
 
