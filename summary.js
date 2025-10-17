048f92f6536f42e4ced66b87ee860675a5674854
Min <kendamamin1480@gmail.com>
2025-10-17 00:30:51 -1000

    New pics

--- summary.js
+++ summary.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const Jimp = require("jimp");
 
-const inputFolder = "src/assets/kekaimoment";
+const inputFolder = "src/assets/new";
 
 const widthStats = {};
 const extensionCounts = {};
