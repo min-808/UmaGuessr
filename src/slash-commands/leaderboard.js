22006a7a84d328119cb2cdae1c8df801f3f32151
Min <kendamamin1480@gmail.com>
2025-10-19 22:12:34 -1000

    Fixed spacing

--- src/slash-commands/leaderboard.js
+++ src/slash-commands/leaderboard.js
@@ -80,134 +80,134 @@ module.exports = {
             title = "Global"
         }
 
-            const client = new MongoClient(uri);
-            const database = client.db("uma");
-            const ids = database.collection("profiles");
+        const client = new MongoClient(uri);
+        const database = client.db("uma");
+        const ids = database.collection("profiles");
 
-            const options = {
-                projection: {
-                    _id: 0,
-                    discord_id: 1,
-                    username: 1,
-                    points: 1,
-                    wins: 1,
-                    streak: 1,
-                    points_today: 1,
-                    wins_today: 1,
-                    top_streak: 1,
-                    times: 1,
-                    quickest_answer: 1,
+        const options = {
+            projection: {
+                _id: 0,
+                discord_id: 1,
+                username: 1,
+                points: 1,
+                wins: 1,
+                streak: 1,
+                points_today: 1,
+                wins_today: 1,
+                top_streak: 1,
+                times: 1,
+                quickest_answer: 1,
+            }
+        };
+
+        const selectedOption = type
+        var userRank
+        let listOfDocuments
+
+        if (range == "global") {
+            listOfDocuments = await ids.find({}, options).toArray()
+        } else {
+            listOfDocuments = await ids.find({ guilds: serverId }, options).toArray()
+        }
+
+        if (selectedOption == "times") { // smallest avg first
+            listOfDocuments.sort((a, b) => {
+                // If not an array, empty, or too short (< 5), replace with [Infinity]
+                let aTimes = Array.isArray(a.times) && a.times.length >= 5 ? a.times : [Infinity]
+                let bTimes = Array.isArray(b.times) && b.times.length >= 5 ? b.times : [Infinity]
+
+                let aAvg = aTimes.reduce((sum, t) => sum + t, 0) / aTimes.length
+                let bAvg = bTimes.reduce((sum, t) => sum + t, 0) / bTimes.length
+
+                return aAvg - bAvg
+            })
+
+            userRank = listOfDocuments.findIndex(c => c.discord_id.toString() === interaction.user.id.toString()) + 1
+        } else if (selectedOption == "quickest_answer") {
+            listOfDocuments.sort((a, b) => {
+                let aTime = a[selectedOption] === 0 ? Infinity : a[selectedOption]
+                let bTime = b[selectedOption] === 0 ? Infinity : b[selectedOption]
+
+                return aTime - bTime
+            })
+
+            userRank = listOfDocuments.findIndex(c => c.discord_id.toString() === interaction.user.id.toString()) + 1
+        } else {
+            listOfDocuments.sort((a, b) => b[selectedOption] - a[selectedOption])
+            userRank = listOfDocuments.findIndex(c => c.discord_id.toString() === interaction.user.id.toString()) + 1
+        }
+
+        /*
+
+        // This process takes forever, scrapped to cache usernames in daily cron job instead
+        for (let doc of listOfDocuments) {
+            const foundID = doc.discord_id;
+
+            const response = await fetch(`https://discord.com/api/v10/users/${foundID}`, {
+                headers: {
+                    'Authorization': 'Bot ' + process.env.TOKEN
                 }
-            };
+            });
 
-            const selectedOption = type
-            var userRank
-            let listOfDocuments
+            const parse = await response.json();
+            let returnedUsername = String(parse?.username ?? 'Unknown');
+            // removed discriminators check since they were phased out
 
-            if (range == "global") {
-                listOfDocuments = await ids.find({}, options).toArray()
-            } else {
-                listOfDocuments = await ids.find({ guilds: serverId }, options).toArray()
-            }
+            doc.discord_id = returnedUsername;
+        }
+        */
 
-            if (selectedOption == "times") { // smallest avg first
-                listOfDocuments.sort((a, b) => {
-                    // If not an array, empty, or too short (< 5), replace with [Infinity]
-                    let aTimes = Array.isArray(a.times) && a.times.length >= 5 ? a.times : [Infinity]
-                    let bTimes = Array.isArray(b.times) && b.times.length >= 5 ? b.times : [Infinity]
+        const permaSize = listOfDocuments.length;
+        const showPerPage = 5;
+        let totalCount = 1;
+        let pages = Math.ceil(listOfDocuments.length / showPerPage);
+        const embeds = [];
+        let displayValue;
 
-                    let aAvg = aTimes.reduce((sum, t) => sum + t, 0) / aTimes.length
-                    let bAvg = bTimes.reduce((sum, t) => sum + t, 0) / bTimes.length
+        for (let i = 0; i < pages; i++) {
+            const embed = new EmbedBuilder()
+                .setTitle(`**${title} Leaderboard (${proper})  |  Page (${i + 1}/${pages})**`)
+                .setColor('LightGrey')
 
-                    return aAvg - bAvg
-                })
+            for (let j = 0; j < showPerPage && listOfDocuments.length > 0; j++) {
+                const entry = listOfDocuments.shift();
 
-                userRank = listOfDocuments.findIndex(c => c.discord_id.toString() === interaction.user.id.toString()) + 1
-            } else if (selectedOption == "quickest_answer") {
-                listOfDocuments.sort((a, b) => {
-                    let aTime = a[selectedOption] === 0 ? Infinity : a[selectedOption]
-                    let bTime = b[selectedOption] === 0 ? Infinity : b[selectedOption]
-
-                    return aTime - bTime
-                })
-
-                userRank = listOfDocuments.findIndex(c => c.discord_id.toString() === interaction.user.id.toString()) + 1
-            } else {
-                listOfDocuments.sort((a, b) => b[selectedOption] - a[selectedOption])
-                userRank = listOfDocuments.findIndex(c => c.discord_id.toString() === interaction.user.id.toString()) + 1
-            }
-
-            /*
-
-            // This process takes forever, scrapped to cache usernames in daily cron job instead
-            for (let doc of listOfDocuments) {
-                const foundID = doc.discord_id;
-
-                const response = await fetch(`https://discord.com/api/v10/users/${foundID}`, {
-                    headers: {
-                        'Authorization': 'Bot ' + process.env.TOKEN
+                if (selectedOption == "times") {
+                    if (!Array.isArray(entry.times) || entry.times.length === 0 || entry.times.length < 5) {
+                        displayValue = 'n/a'
+                        countType = ''
+                    } else {
+                        const avg = entry.times.reduce((sum, t) => sum + t, 0) / entry.times.length;
+                        displayValue = (avg / 1000).toFixed(2)
+                        countType = "sec"
                     }
-                });
-
-                const parse = await response.json();
-                let returnedUsername = String(parse?.username ?? 'Unknown');
-                // removed discriminators check since they were phased out
-
-                doc.discord_id = returnedUsername;
-            }
-            */
-
-            const permaSize = listOfDocuments.length;
-            const showPerPage = 5;
-            let totalCount = 1;
-            let pages = Math.ceil(listOfDocuments.length / showPerPage);
-            const embeds = [];
-            let displayValue;
-
-            for (let i = 0; i < pages; i++) {
-                const embed = new EmbedBuilder()
-                    .setTitle(`**${title} Leaderboard (${proper})  |  Page (${i + 1}/${pages})**`)
-                    .setColor('LightGrey')
-
-                for (let j = 0; j < showPerPage && listOfDocuments.length > 0; j++) {
-                    const entry = listOfDocuments.shift();
-
-                    if (selectedOption == "times") {
-                        if (!Array.isArray(entry.times) || entry.times.length === 0 || entry.times.length < 5) {
+                } else if (selectedOption == "quickest_answer") {
+                    if (entry.quickest_answer === 0) {
                             displayValue = 'n/a'
                             countType = ''
-                        } else {
-                            const avg = entry.times.reduce((sum, t) => sum + t, 0) / entry.times.length;
-                            displayValue = (avg / 1000).toFixed(2)
-                            countType = "sec"
-                        }
-                    } else if (selectedOption == "quickest_answer") {
-                        if (entry.quickest_answer === 0) {
-                                displayValue = 'n/a'
-                                countType = ''
-                        } else {
-                            displayValue = (entry.quickest_answer / 1000).toFixed(2)
-                            countType = "sec"
-                        }
                     } else {
-                        displayValue = entry[selectedOption]
+                        displayValue = (entry.quickest_answer / 1000).toFixed(2)
+                        countType = "sec"
                     }
-
-                    embed.addFields({
-                        name: "",
-                        value: `${totalCount}. **${entry.username}** - ${displayValue} ${countType}`
-                    });
-                    totalCount++;
+                } else {
+                    displayValue = entry[selectedOption]
                 }
 
-                embed.setFooter({ text: `You are rank #${userRank} // There are ${permaSize} players` });
-                embeds.push({
-                    embed: embed,
-
-                })
+                embed.addFields({
+                    name: "",
+                    value: `${totalCount}. **${entry.username}** - ${displayValue} ${countType}`
+                });
+                totalCount++;
             }
 
-            await buttonPagination(sent, embeds);
+            embed.setFooter({ text: `You are rank #${userRank} // There are ${permaSize} players` });
+            embeds.push({
+                embed: embed,
+
+            })
+        }
+
+        await buttonPagination(sent, embeds);
 
         } catch (error) {
             const msg = error?.rawError?.message || error?.message || String(error);
