5c6fb7aeb870ce0a314cbf51712c39a683bded32
Min <kendamamin1480@gmail.com>
2025-10-28 20:45:38 -1000

    New images

--- summary.js
+++ summary.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const Jimp = require("jimp");
 
-const inputFolder = "src/assets/multi";
+const inputFolder = "src/assets/new/multis";
 
 const widthStats = {};
 const extensionCounts = {};
